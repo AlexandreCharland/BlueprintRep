5665ac8f1a2fcdab68042a03cc1c15402ad9f0d3
Alexandre Charland <Alexandre@Charland.com>
2025-02-11 23:06:29 -0500

    finish for today

--- BlueprintRep/Young.lean
+++ BlueprintRep/Young.lean
@@ -18,11 +18,56 @@ theorem bijYu {μ : YoungDiagram} (Yᵤ : YoungTableau μ) :
   simp
   exact injYu Yᵤ
 
-/-
-def S (μ : YoungDiagram) (T : YoungTableau μ) : Set (Equiv.Perm (Fin μ.card)) :=
-  {x : (Equiv.Perm (Fin μ.card)) | ∀ {i1 i2 j1 j2}, (((i1, j1) ∈ μ) ∧ ((i2, j2) ∈ μ)) → (x (T.entry i1 j1) ≠ T.entry i2 j2 ↔ i1 = i2)}
+def Pu (μ : YoungDiagram) (Yᵤ : YoungTableau μ) : Subgroup (Equiv.Perm (Fin μ.card)) where
+  carrier := {x : (Equiv.Perm (Fin μ.card)) | ∀ {i j : μ.cells}, (x (Yᵤ.entry i) = Yᵤ.entry j) → i.val.snd = j.val.snd}
+  mul_mem' := by
+    intros α β a b i j αβ
+    rw[Equiv.Perm.coe_mul, Function.comp_apply] at αβ
+    have h : ∃! (k : μ.cells), Yᵤ.entry k = β (Yᵤ.entry i) := by
+      have h1 : ∀ (j' : Fin μ.card), ∃! i', Yᵤ.entry i' = j' := by
+        rw[← Function.bijective_iff_existsUnique (Yᵤ.entry)]
+        exact bijYu Yᵤ
+      exact h1 (β (Yᵤ.entry i))
+    obtain ⟨k, hk, _⟩ := h
+    rw[Eq.comm] at hk
+    have ik : i.val.snd = k.val.snd := by exact b hk
+    rw[hk] at αβ
+    have jk : k.val.snd = j.val.snd := by exact a αβ
+    rw[ik,jk]
+  one_mem' := by
+    intros _ _ h
+    rw[Equiv.Perm.coe_one, id_eq, Function.Injective.eq_iff] at h
+    rw[h]
+    exact injYu Yᵤ
+  inv_mem' := by
+    intros _ h1 _ _ h2
+    rw[Eq.comm, Equiv.Perm.eq_inv_iff_eq] at h2
+    rw[Eq.comm]
+    exact h1 h2
 
-def PLambda (μ : YoungDiagram) (T : YoungTableau μ) : Subgroup (Equiv.Perm (Fin μ.card)) where
-  carrier := {x : (Equiv.Perm (Fin μ.card)) | ∀ {i1 i2 j1 j2}, (((i1, j1) ∈ μ) ∧ ((i2, j2) ∈ μ)) → (x (T.entry i1 j1) ≠ T.entry i2 j2 ↔ i1 = i2)}
-  mul_mem :=
--/
+def Qu (μ : YoungDiagram) (Yᵤ : YoungTableau μ) : Subgroup (Equiv.Perm (Fin μ.card)) where
+  carrier := {x : (Equiv.Perm (Fin μ.card)) | ∀ {i j : μ.cells}, (x (Yᵤ.entry i) = Yᵤ.entry j) → i.val.fst = j.val.fst}
+  mul_mem' := by
+    intros α β a b i j αβ
+    rw[Equiv.Perm.coe_mul, Function.comp_apply] at αβ
+    have h : ∃! (k : μ.cells), Yᵤ.entry k = β (Yᵤ.entry i) := by
+      have h1 : ∀ (j' : Fin μ.card), ∃! i', Yᵤ.entry i' = j' := by
+        rw[← Function.bijective_iff_existsUnique (Yᵤ.entry)]
+        exact bijYu Yᵤ
+      exact h1 (β (Yᵤ.entry i))
+    obtain ⟨k, hk, _⟩ := h
+    rw[Eq.comm] at hk
+    have ik : i.val.fst = k.val.fst := by exact b hk
+    rw[hk] at αβ
+    have jk : k.val.fst = j.val.fst := by exact a αβ
+    rw[ik,jk]
+  one_mem' := by
+    intros _ _ h
+    rw[Equiv.Perm.coe_one, id_eq, Function.Injective.eq_iff] at h
+    rw[h]
+    exact injYu Yᵤ
+  inv_mem' := by
+    intros _ h1 _ _ h2
+    rw[Eq.comm, Equiv.Perm.eq_inv_iff_eq] at h2
+    rw[Eq.comm]
+    exact h1 h2
